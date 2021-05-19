262f53affb7f9b3c8ae74888743f05541500caa2
Christopher Rust <christopher.rust@stud.hs-hannover.de>
2021-05-19 16:10:11 +0200

    linearreg integriert

--- localization.py
+++ localization.py
@@ -12,15 +12,21 @@ import minvc as vc
 # read beacon locations
 beacons = pd.read_csv("beacons.csv")
 beacons.set_index('name', inplace=True)
-#display(beacons)
+# display(beacons)
 print(beacons)
 
 # read measurements
+# filename = "project/measurement_proj.p"
+# with open(filename, 'rb') as f:
+#     measurements = pickle.load(f)
+# print(type(measurements))
+# print(measurements)
+
 filename = "measurement1.p"
 with open(filename, 'rb') as f:
     measurements = pickle.load(f)
 print(type(measurements))
-#print(measurements)
+
 
 # select one measurement
 meas = measurements[0]
@@ -41,16 +47,16 @@ def visualize(meas, beacons):
     beacon_dists = meas.get_beacon_dists()
     beacon_dists2d = meas.get_beacon_dists2d()
 
-    rssi_conv = vc.RSSIConverter( c0=-29.0, n=2.3, d0=1.2)
+    rssi_conv = vc.RSSIConverter( c0=-29.0, n=2.3, d0=1.0)
     beacon_rssis = rssi_conv.get_dist(meas.get_beacon_rssis())
 
     print("beacon names: {}".format(beacon_names))
-    for name, dist, dist2d, rssis in zip(beacon_names, beacon_dists, beacon_dists2d, beacon_rssis):
+    for name, dist, dist2d, rssi in zip(beacon_names, beacon_dists, beacon_dists2d, beacon_rssis):
         beacon_location = beacons.loc[name].values
 
         print("beacon: {} x: {} y:{} ".format(name, beacon_location[0], beacon_location[1]))
         # [0:2] nur index 0 bis exkl 2 nehmen
-        ax.add_patch(plt.Circle(beacon_location[0:2], radius=rssis, alpha=0.3, color='g', fill=False))
+        ax.add_patch(plt.Circle(beacon_location[0:2], radius=rssi, alpha=0.3, color='g', fill=False))
         ax.add_patch(plt.Circle(beacon_location[0:2], radius=dist, alpha=0.3, color='orange', fill=False))
         ax.add_patch(plt.Circle(beacon_location[0:2], radius=r_dot, fc='b'))
         plt.annotate("{} dist: {:.2f}".format(name ,dist), beacon_location[0:2] + offset)
@@ -58,12 +64,15 @@ def visualize(meas, beacons):
     plt.legend(["real", "dist2d", "dist", "beacon"])
     plt.show()
 
-beacon_locations = beacons.loc[meas.get_beacon_names()].values
-print("location")
-print(beacon_locations)
+# beacon_locations = beacons.loc[meas.get_beacon_names()].values
+# print("location")
+# print(beacon_locations)
 
 visualize(meas, beacons)
+res = stats.linregress(meas.get_beacon_dists(), meas.get_beacon_rssis())
+print(res)
 
 # scatter plot of RSSI vs. distance
-#plt.scatter(meas.get_beacon_dists(), meas.get_beacon_rssis())
-#plt.show()
+plt.scatter(meas.get_beacon_dists(), meas.get_beacon_rssis())
+plt.plot(meas.get_beacon_dists(), res.intercept + res.slope*meas.get_beacon_dists(), 'r', label='fitted line')
+plt.show()
